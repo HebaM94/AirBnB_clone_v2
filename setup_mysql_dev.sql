df205d649dcbfaea5628977fe162f5decd2950b4
HebaZaki <h_borhan@hotmail.com>
2024-04-16 17:28:00 +0200

    setup MySQL server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- his script creates a MySQL server
+-- This script creates a MySQL server
 
 CREATE DATABASE if NOT EXISTS hbnb_dev_db;
 CREATE USER if NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
