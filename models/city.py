24759e7297b2610121a6ac1ecdc45bd3f5e8d68f
HebaZaki <h_borhan@hotmail.com>
2024-04-16 14:49:32 +0200

    city class

--- models/city.py
+++ models/city.py
@@ -5,5 +5,12 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    def __init__(
+            self,
+            state_id="",
+            name="",
+            *args,
+            **kwargs):
+        """New City instance"""
+        super().__init__(**kwargs)
+        self.name = name
