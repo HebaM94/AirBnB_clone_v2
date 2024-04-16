52471c611efe3b6682a343bf4c4f8db2947407f1
HebaZaki <h_borhan@hotmail.com>
2024-04-16 17:26:56 +0200

    setup MySQL server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,4 +2,8 @@
 
 CREATE DATABASE if NOT EXISTS hbnb_dev_db;
 CREATE USER if NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
