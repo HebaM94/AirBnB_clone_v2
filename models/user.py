b7ea1fdeda2c8e1ce941a966dc716856945ef315
HebaZaki <h_borhan@hotmail.com>
2024-04-16 14:43:41 +0200

    user class

--- models/user.py
+++ models/user.py
@@ -5,7 +5,16 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    def __init__(
+            self, email="",
+            password="",
+            first_name="",
+            last_name="",
+            *args,
+            **kwargs):
+        """New User instance"""
+        super().__init__(**kwargs)
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
