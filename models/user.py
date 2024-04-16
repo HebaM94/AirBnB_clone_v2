068594e903840c55dc0df26c094e1a54d2ff052e
HebaZaki <h_borhan@hotmail.com>
2024-04-16 14:46:43 +0200

    user class

--- models/user.py
+++ models/user.py
@@ -6,7 +6,8 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """Class defining a user"""
     def __init__(
-            self, email="",
+            self,
+            email="",
             password="",
             first_name="",
             last_name="",
