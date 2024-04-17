50f0d57f7cf46b81879a719777af0ee52a7d3fe2
HebaZaki <h_borhan@hotmail.com>
2024-04-17 23:33:28 +0200

    place class

--- models/place.py
+++ models/place.py
@@ -33,8 +33,8 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", cascade="delete", backref="place")
-    amenities = relationship("Amenity", secondary=place_amenity,
-                             viewonly=False)
+    amenities = relationship("Amenity", secondary="place_amenity",
+                             viewonly=False, overlaps="place_amenities")
     amenity_ids = []
 
     if os.getenv("HBNB_TYPE_STORAGE") != "db":
