734a9d005500356e564778f84a5d36ac06fb6f88
HebaZaki <h_borhan@hotmail.com>
2024-04-17 23:57:49 +0200

    testing db file

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,11 +1,26 @@
 import unittest
 from models.base_model import BaseModel
+from models.engine.db_storage import DBStorage
 from models import storage
 import os
 
 
-class TestDbStorage(unittest.TestCase):
+class TestDBStorage(unittest.TestCase):
     """ Class to test the db storage method """
+    def setUp(self):
+        """Set up the DBStorage"""
+        self.db_storage = DBStorage()
+
+    def tearDown(self):
+        """Clean up after each test"""
+        storage.reload()
+
+    def test_all_method(self):
+        """Test the all method"""
+        new_obj = BaseModel()
+        new_obj.save()
+        obj_dict = storage.all()
+        self.assertIn(new_obj, obj_dict.values())
 
     
 if __name__ == '__main__':
