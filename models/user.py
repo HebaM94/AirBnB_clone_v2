76e01043e92169d9d54415d4ac8ce5056e4c809c
HebaZaki <h_borhan@hotmail.com>
2024-04-17 22:27:42 +0200

    user class

--- models/user.py
+++ models/user.py
@@ -17,17 +17,6 @@ class User(BaseModel, Base):
                            backref='user')
 
     """Class defining a user"""
-    def __init__(
-            self,
-            email="",
-            password="",
-            first_name="",
-            last_name="",
-            *args,
-            **kwargs):
+    def __init__(self,*args,**kwargs):
         """New User instance"""
         super().__init__(**kwargs)
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
