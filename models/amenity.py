325185867fdd8c832d54a80c99ea7ae2511551cc
HebaZaki <h_borhan@hotmail.com>
2024-04-17 23:33:21 +0200

    Amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -9,7 +9,7 @@ class Amenity(BaseModel, Base):
     """Class defining Amenity"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
+    place_amenities = relationship("Place", secondary="place_amenity", overlaps="place_amenities")
 
     def __init__(self,*args,**kwargs):
         """New Amenity instance"""
