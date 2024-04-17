e983d86e2886d46551405390ffd99a7a57775fa2
HebaZaki <h_borhan@hotmail.com>
2024-04-17 23:50:06 +0200

    testing db file

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,12 @@
+import unittest
+from models.base_model import BaseModel
+from models import storage
+import os
+
+
+class TestDbStorage(unittest.TestCase):
+    """ Class to test the db storage method """
+
+    
+if __name__ == '__main__':
+    unittest.main()
