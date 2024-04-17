fd499928b1cb88dd1362616e2c2e300a58f245b4
Heba <hebamagde@gmail.com>
2024-04-17 09:51:49 +0200

    restore

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,6 +4,6 @@ CREATE DATABASE if NOT EXISTS hbnb_test_db;
 CREATE USER if NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 
 FLUSH PRIVILEGES;
