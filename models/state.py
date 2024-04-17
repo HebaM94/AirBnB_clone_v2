ef077ed45e434843aaaebfad096740a46e472a93
Heba <hebamagde@gmail.com>
2024-04-17 06:09:34 +0200

    update task 6

--- models/state.py
+++ models/state.py
@@ -3,30 +3,23 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import os
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
-
-    def __init__(
-            self,
-            name="",
-            *args,
-            **kwargs):
-        """New State instance"""
-        super().__init__(**kwargs)
-        self.name = name
-
-    @property
-    def cities(self):
-        """getter attribute cities that returns the list of City instances
-        with state_id equals to the current State.id"""
-        from models import storage
-        cts = []
-        cities = storage.all("City").values()
-        for city in cities:
-            if city.state_id == self.id:
-                cts.append(city)
-        return cts
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else: 
+        @property
+        def cities(self):
+            """getter attribute cities that returns the list of City instances
+            with state_id equals to the current State.id"""
+            from models import storage
+            cts = []
+            cities = storage.all("City").values()
+            for city in cities:
+                if city.state_id == self.id:
+                    cts.append(city)
+            return cts
