740adb94af5f35430529298e8322af2c5fa4d590
Heba <hebamagde@gmail.com>
2024-04-17 08:09:21 +0200

    update 7

--- models/user.py
+++ models/user.py
@@ -11,18 +11,3 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
-    """Class defining a user"""
-    def __init__(
-            self,
-            email="",
-            password="",
-            first_name="",
-            last_name="",
-            *args,
-            **kwargs):
-        """New User instance"""
-        super().__init__(**kwargs)
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
