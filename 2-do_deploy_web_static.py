434fbf8b0a3a3de18460edf43bccc79bedc529a8
Heba <hebamagde@gmail.com>
2024-05-04 04:31:22 +0300

    Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Fabric script that distributes an archive to your web servers,
+using the function do_deploy"""
+
+
+from fabric.api import env, put, run
+import os
+from datetime import datetime
+
+
+env.hosts = ['100.25.31.166', '54.175.6.240']
+env.user = 'ubuntu'
+env.key_filename = '/path/to/your/ssh/private/key'
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to web servers and deploys it"""
+
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        put(archive_path, '/tmp/')
+
+        archive_filename = os.path.basename(archive_path)
+        release_folder = '/data/web_static/releases/{}'.format(
+            archive_filename[:-4])
+        run('mkdir -p {}'.format(release_folder))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_folder))
+
+        run('rm /tmp/{}'.format(archive_filename))
+
+        run('mv {}/web_static/* {}'.format(release_folder, release_folder))
+
+        run('rm -rf {}/web_static'.format(release_folder))
+
+        run('rm -rf /data/web_static/current')
+
+        run('ln -s {} /data/web_static/current'.format(release_folder))
+
+        return True
+    except:
+        return False
