ba3708ff43d40e8879f6c0048c01528f9007d502
Heba <hebamagde@gmail.com>
2024-05-04 05:24:53 +0300

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,7 @@ from datetime import datetime
 
 
 env.hosts = ['100.25.31.166', '54.175.6.240']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
@@ -18,12 +19,14 @@ def do_deploy(archive_path):
         return False
 
     try:
+        for host in env.hosts:
+            print("[{}] Executing task 'do_deploy'".format(host))
+
         put(archive_path, '/tmp/')
 
-        archive_filename = archive_path.split("/")[1]
-        filename = archive_filename.split(".")[0]
-        release_folder = "/data/web_static/releases/" + filename + "/"
-
+        archive_filename = os.path.basename(archive_path)
+        release_folder = '/data/web_static/releases/{}'.format(
+            archive_filename[:-4])
         run('mkdir -p {}'.format(release_folder))
         run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_folder))
 
@@ -38,7 +41,8 @@ def do_deploy(archive_path):
         run('ln -s {} /data/web_static/current'.format(release_folder))
 
         print("New version deployed!")
+        
         return True
     except Exception as e:
-        print("Error:", e)
+        print(e)
         return False
