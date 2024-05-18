d00dab39ecec5319634b2f73bf55500d9aaa24fe
Heba <hebamagde@gmail.com>
2024-05-18 12:44:28 +0300

    C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Hello HBNB! - Start"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """print HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """print C followed by text"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(escape(text))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
