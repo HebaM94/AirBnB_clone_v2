77ef10fb03f43795abed04a069f47a73a7c0835b
Heba <hebamagde@gmail.com>
2024-05-18 13:02:28 +0300

    update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Start a Flask web application"""
 
-from flask import Flask, escape
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -22,7 +22,7 @@ def hbnb():
 def c(text):
     """C followed by text"""
     text = text.replace('_', ' ')
-    return "C {}".format(escape(text))
+    return "C {}".format(text)
 
 
 if __name__ == '__main__':
