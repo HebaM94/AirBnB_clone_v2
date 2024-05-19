34af58bdd2d56cd420737dd91ebc9fec2ad8ca0b
Heba <hebamagde@gmail.com>
2024-05-19 23:52:25 +0300

    update

--- models/place.py
+++ models/place.py
@@ -38,8 +38,7 @@ class Place(BaseModel, Base):
 
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", cascade="delete", backref="place")
-        amenities = relationship("Amenity", secondary="place_amenity",
-                                    viewonly=False, overlaps="place_amenities")
+        amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
     
     else:
         @property
