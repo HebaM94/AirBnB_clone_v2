fcd39cfc037071786cb0076174bfa16c02067fec
Heba <hebamagde@gmail.com>
2024-05-20 05:44:21 +0300

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -19,7 +19,7 @@ def states_list():
     """display a HTML page: (inside the tag BODY) """
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('states_list.html', states=sorted_states)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 if __name__ == '__main__':
